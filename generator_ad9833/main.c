b4a4fbeb3570c841d10e578e8eedf88ce3cfe57e
Serhii Zashchelkin <serhii.zashchelkin@connectwise.com>
2023-02-11 21:23:17 +0100

    Updates

--- generator_ad9833/main.c
+++ generator_ad9833/main.c
@@ -4,6 +4,8 @@
 #include <mcp41_42.h>
 #include <spi_soft.h>
 
+#define DDS_COMMAND_QUEUE_LENGTH 5
+
 const dds_config config =
 {
     .max_frequency = 12500000,
@@ -17,9 +19,9 @@ const dds_config config =
     .supported_modes = (1 << DDS_MODE_SINE) | (1 << DDS_MODE_TRIANGLE) | (1 << DDS_MODE_SQUARE)
 };
 
-static volatile dds_i2c_command command;
-static volatile unsigned char *tx_p;
-static volatile unsigned int command_length, command_ready;
+static dds_i2c_command command[DDS_COMMAND_QUEUE_LENGTH];
+static volatile unsigned char *tx_data_p, *rx_data_p;
+static volatile unsigned int rx_cnt, tx_cnt, current_command;
 
 static void initGPIO(void)
 {
@@ -44,12 +46,14 @@ static void initI2C(void)
 int main(void)
 {
   unsigned short ad9833_cfg, ad9833_divider;
+  dds_i2c_command *cmd;
+  unsigned int command_to_process;
 
   WDTCTL = WDTPW + WDTHOLD;                 // Stop WDT
   initGPIO();
   initI2C();
 
-  command_ready = command_length = 0;
+  command_to_process = current_command = 0;
 
   ad9833_cfg = AD9833_CFG_SLEEP1|AD9833_CFG_SLEEP12;
   ad9833_set_config(0, ad9833_cfg);
@@ -57,15 +61,16 @@ int main(void)
 
   while (1)
   {
-      if (command_ready)
+      while (current_command != command_to_process)
       {
-          switch (command.c3.command)
+          cmd = &command[command_to_process];
+          switch (cmd->c3.command)
           {
           case DDS_COMMAND_SET_FREQUENCY_CODE:
-              ad9833_set_freq0_word(0, command.c8.freq);
+              ad9833_set_freq0_word(0, cmd->c8.freq);
               if (ad9833_cfg & AD9833_CFG_MSBDIV2)
               {
-                  ad9833_divider = command.c8.div;
+                  ad9833_divider = cmd->c8.div;
                   if (ad9833_divider == 1)
                   {
                       if ((ad9833_cfg & AD9833_CFG_MSB) == AD9833_CFG_MSBDIV2)
@@ -76,7 +81,7 @@ int main(void)
               }
               break;
           case DDS_COMMAND_SET_MODE:
-              switch (command.c3.parameter)
+              switch (cmd->c3.parameter)
               {
               case DDS_MODE_SINE:
                 ad9833_update_mode(0, &ad9833_cfg, AD9833_CFG_SINUS);
@@ -93,17 +98,19 @@ int main(void)
               }
               break;
           case DDS_COMMAND_SET_ATTENUATOR:
-              mcp41_42_set(0, MCP41_42_SET_DATA0, command.c4.parameter);
+              mcp41_42_set(0, MCP41_42_SET_DATA0, cmd->c4.parameter);
               break;
           case DDS_COMMAND_ENABLE_OUTPUT:
-              if (command.c3.parameter)
+              if (cmd->c3.parameter)
                   ad9833_cfg &= ~(AD9833_CFG_SLEEP1|AD9833_CFG_SLEEP12);
               else
                   ad9833_cfg |= AD9833_CFG_SLEEP1|AD9833_CFG_SLEEP12;
               ad9833_set_config(0, ad9833_cfg);
               break;
           }
-          command_ready = 0;
+          command_to_process++;
+          if (command_to_process >= DDS_COMMAND_QUEUE_LENGTH)
+            command_to_process = 0;
       }
       __bis_SR_register(CPUOFF + GIE);        // Enter LPM0 w/ interrupts
   }
@@ -113,12 +120,27 @@ int main(void)
 #pragma vector = USCIAB0TX_VECTOR
 __interrupt void USCIAB0TX_ISR(void)
 {
+    volatile unsigned int dummy;
+
     if (IFG2 & UCB0TXIFG) // TX
-        UCB0TXBUF = *tx_p++;
+    {
+        if (tx_cnt < sizeof(dds_config))
+        {
+            UCB0TXBUF = *tx_data_p++;
+            tx_cnt++;
+        }
+        else
+            UCB0TXBUF = 0;
+    }
     else
     {
-        if (!command_ready && command_length < sizeof(command))
-            command.bytes[command_length++] = UCB0RXBUF;
+        if (rx_cnt < sizeof(dds_i2c_command))
+        {
+          *rx_data_p++ = UCB0RXBUF;
+          rx_cnt++;
+        }
+        else
+          dummy = UCB0RXBUF;
     }
 }
 
@@ -128,17 +150,22 @@ __interrupt void usci_i2c_state_isr(void)
 {
     unsigned char stat = UCB0STAT;
     UCB0STAT &= ~(UCSTTIFG | UCSTPIFG);        // Clear start/stop condition int flag
-    if (stat & UCSTTIFG)
+    if (stat & UCSTTIFG) // start
     {
-        command_length = 0; // start
-        tx_p = (unsigned char*)&config;
         P1OUT |= BIT0;
+        rx_data_p = command[current_command].bytes;
+        tx_data_p = (unsigned char*)&config;
+        rx_cnt = tx_cnt = 0;
     }
     else // stop
     {
-        if (command_length)
-            command_ready = 1;
         P1OUT &= ~BIT0;
+        if (rx_cnt)
+        {
+          current_command++;
+          if (current_command >= DDS_COMMAND_QUEUE_LENGTH)
+            current_command = 0;
+        }
     }
     __bic_SR_register_on_exit(CPUOFF);        // Exit LPM0
 }
